71ccaf23de6ed328af8d5633b1f1622b49eea450
RodrigoCantorV <rcvunicun@gmail.com>
2023-04-23 14:26:08 -0500

    Codigo Controles del jugador

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -9,15 +9,13 @@ public class MovePlayer : MonoBehaviour
     public bool isGround = true;
 
     private float speed = 5.0f;
-    private float horizontalInput;
-    private float forwardInput;
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float verticalInput;
 
     private Rigidbody playerRb;
     public Animator playerAnimator;
 
-    Vector3 newRotation = new Vector3(0, 10, 0);
 
-    
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -27,66 +25,66 @@ public class MovePlayer : MonoBehaviour
     void Update()
     {
         //this is we get player input
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal") * speed;
+        verticalInput = Input.GetAxis("Vertical") * speed;
 
         // move the player
-        transform.Translate(Vector3.left * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * horizontalInput);
-        if(Input.GetKeyDown(KeyCode.UpArrow) 
-        || Input.GetKeyDown(KeyCode.DownArrow)
-        || Input.GetKeyDown(KeyCode.LeftArrow)
-        || Input.GetKeyDown(KeyCode.RightArrow)
-        || Input.GetKeyDown(KeyCode.W)
-        || Input.GetKeyDown(KeyCode.S)
-        || Input.GetKeyDown(KeyCode.A)
-        || Input.GetKeyDown(KeyCode.D))
-        {
-            playerAnimator.SetBool("Walk", true);
-        }
+        MoverJugador();
 
-        if(Input.GetKeyUp(KeyCode.UpArrow) 
-        || Input.GetKeyUp(KeyCode.DownArrow)
-        || Input.GetKeyUp(KeyCode.LeftArrow)
-        || Input.GetKeyUp(KeyCode.RightArrow)
-        || Input.GetKeyUp(KeyCode.W)
-        || Input.GetKeyUp(KeyCode.S)
-        || Input.GetKeyUp(KeyCode.A)
-        || Input.GetKeyUp(KeyCode.D))
-        {
-            playerAnimator.SetBool("Walk", false);
-        }
+        EjecutarAnimacionCaminar();
 
+        Saltar();
 
-
-
-        if(Input.GetKeyDown(KeyCode.R)){
-            transform.eulerAngles += newRotation;
-        }
-
-        // jump the player
-        if(Input.GetKeyDown(KeyCode.Space) && isGround)
-        {
-                        playerAnimator.SetBool("Jump", true);
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isGround = false;
-        }
-        
-        if(Input.GetKeyUp(KeyCode.Space)){
-            playerAnimator.SetBool("Jump", false);
-        }
     }
 
     // collision detection
-    private void OnCollisionEnter(Collision collision){
-        if(collision.gameObject.CompareTag("Ground")){
-        isGround = true;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
         }
         /*
         if(collision.gameObject.CompareTag("reload")){
             transform.position = new Vector3(3, 7,119);
         }
         */
+    }
+
+    void MoverJugador()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput);
+        transform.Rotate(new Vector3(0f, horizontalInput, 0f).normalized);
+    }
+
+    void Saltar()
+    {
+        // jump the player
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAnimator.SetBool("Jump", true);
+           
+            
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isGround = false;
+            playerAnimator.SetBool("Jump", false);
+        }
+    }
+
+    void EjecutarAnimacionCaminar()
+    {
+        if (verticalInput == 0 && horizontalInput == 0)
+        {
+            playerAnimator.SetBool("Walk", false);
+        }
+        else if (verticalInput != 0 || horizontalInput != 0)
+        {
+            playerAnimator.SetBool("Walk", true);
+        }
     }
 
 }
